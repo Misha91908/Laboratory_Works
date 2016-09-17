a181d3d7047e1835326e843ea194800bd73200c8
Misha91908 <misha91908@gmail.com>
2016-09-17 15:22:06 +0300

    update a program

--- graph/src.py
+++ graph/src.py
@@ -1,23 +1,41 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+def movingaverage (values, window):
+    weights = np.repeat(1.0, window)/window
+    sma = np.convolve(values, weights, 'valid')
+    return sma
 
 f = open('finalLog.txt','r')
 xx = []
 yy = []
+k = 0
+ss = []
+ss1 = []
 for line in f:
      s = line.split(':')
      x = s[2].split(' ')
-     xx.append(x[0])
+     xx.append(float(x[0]))
      y = s[3].split(' ')
-     yy.append(y[0])
+     yy.append(float(y[0]))
+     k = k+1
+i = 1
+
+for t in range(2403):
+     ss.append(xx[i])
+     i = i + 2
+
+xMA = movingaverage(ss,89)
 
 fig = plt.figure()
 plt.subplot(211)
-plt.plot(xx)
+plt.plot(ss[len(ss)-len(xMA):],color="red")
 plt.title('Current')
 plt.grid()
 
 plt.subplot(212)
 plt.plot(yy)
+plt.plot(ss1)
 plt.title('Voltage')
 plt.grid()
 
