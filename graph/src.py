3afa374639ed382f4931f5ff0bdab7e5a2fcaa09
misha919081 <misha91908@gmail.com>
2016-09-20 23:15:15 +0300

    update code

--- graph/src.py
+++ graph/src.py
@@ -1,35 +1,30 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-def movingaverage (values, window):
-    weights = np.repeat(1.0, window)/window
-    sma = np.convolve(values, weights, 'valid')
-    return sma
-
-f = open('finalLog.txt','r')
+f = open('finalLog.txt', 'r')
 xx = []
 yy = []
 k = 0
 ss = []
 ss1 = []
+alpha = float(input())
 for line in f:
-     s = line.split(':')
-     x = s[2].split(' ')
-     xx.append(float(x[0]))
-     y = s[3].split(' ')
-     yy.append(float(y[0]))
-     k = k+1
-i = 1
+    s = line.split(':')
+    x = s[2].split(' ')
+    xx.append(float(x[0]))
+    y = s[3].split(' ')
+    yy.append(float(y[0]))
+    k = k + 1
 
-for t in range(2403):
-     ss.append(xx[i])
-     i = i + 2
+tt = int(input())
 
-xMA = movingaverage(ss,89)
+for t in range(int(k)-tt-1):
+
+    for r in range(tt-1):
+        x[r+1] = alpha*x[r+1]+(1.0 - alpha)*x[r]
 
 fig = plt.figure()
 plt.subplot(211)
-plt.plot(ss[len(ss)-len(xMA):],color="red")
+plt.plot(ss)
 plt.title('Current')
 plt.grid()
 
