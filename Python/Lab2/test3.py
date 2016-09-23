2f842c8d6ce0f658856a21b0b7978b6dba7cd8ed
Misha91908 <caplin1999@yandex.ru>
2016-09-23 16:25:19 +0300

    добавил комментарии

--- Python/Lab2/test3.py
+++ Python/Lab2/test3.py
@@ -2,6 +2,6 @@ import math
 
 x = float(input())
 
-if (x>=-1) and (x<=1):
+if (x>=-1) and (x<=1): # если введенный х принадлежит промежутку [-1;1], то считатет функцию
     f = math.acos(x)
     print("f = ",f)
