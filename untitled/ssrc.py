1548f25ff98bc529fd031a7821c130f2f13cdc31
Misha91908 <misha91908@gmail.com>
2016-11-23 17:21:06 +0300

    update program

--- untitled/ssrc.py
+++ untitled/ssrc.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as ax3d
+from math import sqrt
+
 f = open('qwe.txt', 'r')
 
 dat = []
@@ -31,12 +35,72 @@ for i in range(len(dat) - 1):
 
 ttt = []
 tt = []
+t = []
 
 for i in range(len(dat) - 1):
-    t = dat[i].split('A')
-    t = t[1].split('B')
-    t[1] = t[1].split('C')
-    tt.append(t[0])
-    tt.append(t[1][0])
-    tt.append(t[1][1])
-print(data)
+    dat[i] = dat[i].replace('A',':')
+    dat[i] = dat[i].replace('B', ':')
+    dat[i] = dat[i].replace('C', ':')
+    data.append(dat[i].split(':'))
+    data[i].pop(0)
+
+for i in range(len(dat) - 1):
+    t.append(data[i][0])
+    tt.append(data[i][1])
+    ttt.append(data[i][2])
+
+for i in range(len(dat) - 1):
+    ttt[i] = ttt[i].replace('\n',' ')
+    ttt[i] = ttt[i].rstrip()
+    t[i] = float(t[i])/300
+    tt[i] = float(tt[i])/300
+    ttt[i] = float(ttt[i])/300
+
+track = 0.0
+length = 0.0
+
+for i in range(len(dat) - 2):
+    vecx = t[i+1] - t[i]
+    vecy = tt[i + 1] - tt[i]
+    vecz = ttt[i + 1] - ttt[i]
+    length = sqrt(vecx*vecx+vecy*vecy+vecz*vecz)
+    track = track + length
+
+print("общее расстояние =",track)
+p = len(t)
+
+for kk in range(p - 99):
+    i = 0
+    for tp in range(99):
+        i = i+1
+        t[kk] = t[kk] + t[kk + i]
+        tt[kk] = tt[kk] + tt[kk + i]
+        ttt[kk] = ttt[kk] + ttt[kk + i]
+    t[kk] = t[kk] / 100
+    tt[kk] = tt[kk] / 100
+    ttt[kk] = ttt[kk] / 100
+
+fig = plt.figure()
+ax = ax3d.Axes3D(fig)
+ax.plot(t,tt,ttt,color='green')
+ax.scatter(t[0],tt[0],ttt[0])
+ax.scatter(t[p-1],tt[p-1],ttt[p-1],color='red')
+
+fig1 = plt.figure()
+plt.subplot(311)
+plt.plot(t)
+plt.title('Axelleration(x)')
+plt.grid()
+
+plt.subplot(312)
+plt.plot(tt)
+plt.title('Axelleration(y)')
+plt.grid()
+
+plt.subplot(313)
+plt.plot(ttt)
+plt.title('Axelleration(z)')
+plt.grid()
+
+print(fig1.axes)
+plt.show()
